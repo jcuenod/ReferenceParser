f02036188de0c33839a0e0af88901f398f36f5d6
jcuenod <j3frea+gitkraken@gmail.com>
2018-03-31 15:04:23 -0500

    Support more edge cases and default to null for book

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import bookDetails from './data/bookDetails'
 import generousBookNames from './data/generousBookNames'
 
 interface IReferenceObject {
-    book:string;
+    book:string|null;
     chapter:number|null;
     verse:number|null;
 }
@@ -10,14 +10,13 @@ interface IReferenceObject {
 class ReferenceParser {
     private defaults:IReferenceObject
 
-    constructor({defaults = { book: "Genesis", chapter: null, verse: null }}:{defaults?: IReferenceObject}={}) {
+    constructor({defaults = { book: null, chapter: null, verse: null }}:{defaults?: IReferenceObject}={}) {
         let { book, chapter, verse } = defaults
         this.defaults = { book, chapter, verse }
     }
 
     public parse(referenceString:string):IReferenceObject|false {
         const matches = referenceString.match(/((?:(?:\d)[^a-zA-Z\d\s:]*)?[a-zA-Z-_\s]+)([^a-zA-Z\d:]*(\d+)(\D*(\d+))?)?/)
-
         return matches ? {
             book: this._matchBook(matches[1]) || this.defaults.book,
             chapter: matches[3] ? +matches[3] : this.defaults.chapter,
@@ -36,19 +35,32 @@ class ReferenceParser {
         // now try use regex to guess (return on first match)
         const bookNames = bookDetails.map(b => b.name)
         // let's try a regex on the starting characters of book names
-        const r1 = new RegExp(`^${urlBook}.*`, "i")
+        const r1 = new RegExp(`^${possibleKey}.*`, "i")
         const possibleMatch = bookNames.reduce((a:string|false, v:string) => {
             if (a) return a
             return r1.test(v) ? v : a
         }, false)
         if (possibleMatch) return possibleMatch
+        // and if that didn't work, let's try on the generous booknames...
+        const possibleMatch2 = Object.keys(generousBookNames).reduce((a:string|false, v:string) => {
+            if (a) return a
+            return r1.test(v) ? generousBookNames[v] : a
+        }, false)
+        if (possibleMatch2) return possibleMatch2
 
         // this is a pretty promiscuous guess but it works on stuff like "1kgs"
-        const urlArray = urlBook.split("")
+        const urlArray = possibleKey.split("")
         const r2 = new RegExp("^" + urlArray.join(".*"), "i")
-        return bookNames.reduce((a:string|false, v:string) => {
+        const possibleMatch3 = bookNames.reduce((a:string|false, v:string) => {
             if (a) return a
             return r2.test(v) ? v : a
+        }, false)
+        if (possibleMatch3) return possibleMatch3
+        // Okay, we're really having a hard time with this one,
+        // we'll have a last ditch try with generous book names
+        return Object.keys(generousBookNames).reduce((a:string|false, v:string) => {
+            if (a) return a
+            return r2.test(v) ? generousBookNames[v] : a
         }, false)
     }
 }
